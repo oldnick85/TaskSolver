6b7eb9f5d3c7d8faa556e5a967f23f121f389543
Nikita <tyapin_nikita@mail.ru>
2020-08-02 21:44:29 +0300

    [add] files for command line mode

---
+++ code/lispfunc.lisp
@@ -0,0 +1,7 @@
+(defvar text_task ())
+(defun $solve_q_eq (a b c)
+	(($MPLUS) a b c)
+)
+(defun $foo (x) (format t "~a~%" x))
+(defun $readlinestdio () (read-line))
+;(defun $compile_tex_file (fname) (EXT:SHELL (format nil "pdflatex ~a.tex" fname)))
